db037e20a0e510511fde838b50214dfefb2dd8db
pyo i seul <isl1089@naver.com>
2020-11-30 10:21:57 +0900

    test

---
+++ src/com/javaex/ex14/Person.java
@@ -0,0 +1,54 @@
+package com.javaex.ex14;
+
+public class Person {
+
+		//필드
+	
+		private String name;
+		private int age;
+		
+		
+		//생성자
+		
+		public Person() {
+		}
+		
+		public Person(String name, int age) {
+			super();
+			this.name = name;
+			this.age = age;
+		}
+		
+
+		//메소드 - getter,setter
+		
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		
+		//메소드 일반
+		
+		
+		
+		
+		
+		@Override
+		public String toString() {
+			return "Person [name=" + name + ", age=" + age + "]";
+		}
+
+		
+		
+	
+}
